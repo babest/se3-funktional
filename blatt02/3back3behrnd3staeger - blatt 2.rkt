a15374770d51c8b9ed40b07fe630e4f3c8f49a4c
nstaeger <mail@nstaeger.de>
2014-10-30 09:35:01 +0100

    blatt02 init

---
+++ blatt02/3back3behrnd3staeger - blatt 2.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+#|
+  SE3 Funktionale Programmierung
+  WiSe 2014, Uni Hamburg
+  Aufgabenblatt 02
+  Eingereicht von
+    Timon Back (3back)
+    Fabian Behrendt (3behrend)
+    Nicolai Stäger (3staeger)
+
+  Beispielaufrufe befinden sich immer am Ende der Aufgabe
+  und können einfach auskommentiert werden.
+|#
+
+
+
+; ##############################################################################
+; ## Aufgabe 1.1 ###############################################################
+; ##############################################################################
