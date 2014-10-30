3a5b01ecfbed2d64fd82c72ce77571cc2186e4e3
Timon Back <3back@informatik.uni-hamburg.de>
2014-10-30 09:40:00 +0100

    Aufgabe 1.1

--- blatt02/3back3behrnd3staeger - blatt 2.rkt
+++ blatt02/3back3behrnd3staeger - blatt 2.rkt
@@ -18,3 +18,34 @@
 ; ##############################################################################
 ; ## Aufgabe 1.1 ###############################################################
 ; ##############################################################################
+
+(define miau 'Plueschi )
+(define katze miau)
+(define tiger 'miau)
+(define (welcherNameGiltWo PersonA PersonB)
+  (
+   let
+      (
+       (PersonA 'Sam)
+       (PersonC PersonA )
+       )
+    PersonC
+    )
+  )
+
+(define xs1 '(0 2 3 miau katze ))
+(define xs2 (list miau katze ))
+(define xs3 (cons katze miau ))
+
+miau
+katze
+tiger
+(quote katze)
+;(eval tiger)
+;(eval katze)
+;(eval 'tiger)
+(welcherNameGiltWo 'harry 'potter)
+(cdddr xs1)
+(cdr xs2)
+(cdr xs3)
+;(eval (sqrt 3))
