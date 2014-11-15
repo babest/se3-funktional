5461e93b6c6e0758b4649bf3e02637a88cea7971
3back <3back@informatik.uni-hamburg.de>
2014-11-15 13:32:45 +0100

    Aufgabe 2. Problem mit der Flaggenanzeige

--- blatt03/3back3behrnd3staeger - blatt 3.rkt
+++ blatt03/3back3behrnd3staeger - blatt 3.rkt
@@ -65,20 +65,26 @@
 ; Die Liste so anzulegen, hat den Vorteil, dass sie assoziativ ist (es kann mit assoc darauf zugegriffen werden.
 ; Dadurch kann mit einem Schlüssel auf den Wert zugeriffen werden (map).
 
+
 ; ##############################################################################
 ; ## Aufgabe 1.2 ###############################################################
 ; ##############################################################################
 
-; Gibt den Schlüssel zu einem Symbol aus 'BuchstabierTafel' zurück
+; Gibt den Schlüssel zu einem Char aus 'BuchstabierTafel' zurück
 (define (Buchstabe->tafelwort Buchstabe)
   (
-   cdr (assoc Buchstabe BuchstabierTafel)
-       )
+   car
+   (
+    cdr (assoc Buchstabe BuchstabierTafel)
+        )
+   )
   )
 
 ;(Buchstabe->tafelwort #\B) ; -> '(Bravo)
 ;(Buchstabe->tafelwort 1)   ; -> '(Unaone)
 ;(Buchstabe->tafelwort ".") ; -> '(Stop)
+
+
 
 ; ##############################################################################
 ; ## Aufgabe 1.3 ###############################################################
@@ -89,15 +95,15 @@
    (
     if (char? buchstabe)
        (
-       ; Buchstabe ist ein Char (vielleicht ein kleiner Buchstabe?)
-           if (char-lower-case? buchstabe) 
-              ; b ist klein geschrieben.
-              ; Dann b zu einem Großbuchstaben machen.
-              ; Kann dann an die Buchstabe->tafelwort übergeben werden
-              (char-upcase buchstabe)
-              ; b ist bereits groß geschrieben. Parameter kann einfach weiter gereicht werden
-              buchstabe
-          )
+        ; Buchstabe ist ein Char (vielleicht ein kleiner Buchstabe?)
+        if (char-lower-case? buchstabe) 
+           ; b ist klein geschrieben.
+           ; Dann b zu einem Großbuchstaben machen.
+           ; Kann dann an die Buchstabe->tafelwort übergeben werden
+           (char-upcase buchstabe)
+           ; b ist bereits groß geschrieben. Parameter kann einfach weiter gereicht werden
+           buchstabe
+           )
        ; Buchstabe ist kein Char (also eine Zahl oder . ,). Parameter kann einfach weiter gereicht werden.
        buchstabe
        )
@@ -110,6 +116,7 @@
 ;(buchstabe->tafelwort ".") ; -> '(Stop)
 
 
+
 ; ##############################################################################
 ; ## Aufgabe 1.4 ###############################################################
 ; ##############################################################################
@@ -118,31 +125,37 @@
 (define (charlist->tafelwort charlist) ; charList = Die List der Buchstaben (char) die umgewandelt werden sollen.
   (
    if (empty? charlist)
-   ; Es gibt keine weiteren Buchstaben, also eine leere Liste zurückgeben
-   '()
-   ; Sonst:
-   (
-    ; Eine Liste erstellen aus:
-    cons 
-    ; Dem aktuellen Buchstaben, welcher in das tafelwort 'übersetzt' werden muss
-    (buchstabe->tafelwort 
-     ; unter Berücksichtigung des Parameters, welcher der erste Eintrag in der Liste ist.
-     (car charlist)
-     )
-    ; Zweites Element der neuen Liste ist der rekursive Aufruf mit dem Rest der Liste
-    (charlist->tafelwort (cdr charlist))
-    )
-   )
+      ; Es gibt keine weiteren Buchstaben, also eine leere Liste zurückgeben
+      '()
+      ; Sonst:
+      (
+       ; Eine Liste erstellen aus:
+       cons 
+       ; Dem aktuellen Buchstaben, welcher in das tafelwort 'übersetzt' werden muss
+       (buchstabe->tafelwort 
+        ; unter Berücksichtigung des Parameters, welcher der erste Eintrag in der Liste ist.
+        (car charlist)
+        )
+       ; Zweites Element der neuen Liste ist der rekursive Aufruf mit dem Rest der Liste
+       (charlist->tafelwort (cdr charlist))
+       )
+      )
   )
 
 ; Wandelt einen String in eine Liste von Chars
 (define (wort->tafelwort wort) ; wort = Ein String, der 'übersetzt' wird.
   (
    charlist->tafelwort (string->list wort)
-   )
+                       )
   )
 
 ;(wort->tafelwort "Racket") ; '((Romeo) (Alfa) (Charlie) (Kilo) (Echo) (Tango))
+
+
+
+
+
+
 
 
 
@@ -151,3 +164,95 @@
 ; ##############################################################################
 ; ## Aufgabe 2.1 ###############################################################
 ; ##############################################################################
+
+(define FlaggenTafel 
+  '(
+    (#\A A)
+    (#\B B)
+    (#\C C)
+    (#\D D)
+    (#\E E)
+    (#\F F)
+    (#\G G)
+    (#\H H)
+    (#\I I)
+    (#\J J)
+    (#\K K)
+    (#\L L)
+    (#\M M)
+    (#\N N)
+    (#\O O)
+    (#\P P)
+    (#\Q Q)
+    (#\R R)
+    (#\S S)
+    (#\T T)
+    (#\U U)
+    (#\V V)
+    (#\W W)
+    (#\X X)
+    (#\Y Y)
+    (#\Z Z)
+    (0 Z0)
+    (1 Z1)
+    (2 Z2)
+    (3 Z3)
+    (4 Z4)
+    (5 Z5)
+    (6 Z6)
+    (7 Z7)
+    (8 Z8)
+    (9 Z9)
+    )
+  )
+; Diese Assoziativliste speichert zu jedem Buchstaben einen Schlüssel.
+; Die Liste so anzulegen, hat den Vorteil, dass sie assoziativ ist (es kann mit assoc darauf zugegriffen werden.
+; Dadurch kann mit einem Schlüssel auf den Wert zugeriffen werden (map).
+
+
+; ##############################################################################
+; ## Aufgabe 2.2 ###############################################################
+; ##############################################################################
+
+; Gibt den Schlüssel zu einem Char aus FlaggenTafel zurück
+(define (Buchstabe->flagge Buchstabe)
+  (car
+   (cdr (assoc Buchstabe FlaggenTafel))
+   )
+  )
+
+; (Buchstabe->flagge #\B)
+
+; ##############################################################################
+; ## Aufgabe 2.3 ###############################################################
+; ##############################################################################
+
+; Wandelt eine Liste von Chars in die dazugehörigen Buchstabentafel-Liste (rekursive Funktion)
+(define (charlist->flaggenwort charlist) ; charList = Die List der Buchstaben (char) die umgewandelt werden sollen.
+  (
+   if (empty? charlist)
+      ; Es gibt keine weiteren Buchstaben, also eine leere Liste zurückgeben
+      '()
+      ; Sonst:
+      (
+       ; Eine Liste erstellen aus:
+       cons 
+       ; Dem aktuellen Buchstaben, welcher in das tafelwort 'übersetzt' werden muss
+       (Buchstabe->flagge 
+        ; unter Berücksichtigung des Parameters, welcher der erste Eintrag in der Liste ist.
+        (car charlist)
+        )
+       ; Zweites Element der neuen Liste ist der rekursive Aufruf mit dem Rest der Liste
+       (charlist->flaggenwort (cdr charlist))
+       )
+      )
+  )
+
+; Wandelt einen String in eine Liste von Chars
+(define (wort->flaggenwort wort) ; wort = Ein String, der 'übersetzt' wird.
+  (
+   charlist->flaggenwort (string->list wort)
+                         )
+  )
+
+;(wort->flaggenwort "RACKET") ; '((Romeo) (Alfa) (Charlie) (Kilo) (Echo) (Tango))
