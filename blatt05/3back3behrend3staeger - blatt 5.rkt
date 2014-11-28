1d8d54db682487feca6dd7e4f835a6fdda30859d
likesmusik <3back@informatik.uni-hamburg.de>
2014-11-28 22:48:37 +0100

    Teil 2 von Aufgabe 1 bearbeitet. TODO: Strukturerklaerung schreiben

--- blatt05/3back3behrend3staeger - blatt 5.rkt
+++ blatt05/3back3behrend3staeger - blatt 5.rkt
@@ -22,55 +22,38 @@
 ; ## Aufgabe 1 #################################################################
 ; ##############################################################################
 
+; TODO: Erklärung der Struktur
+
+
+
 (require se3-bib/butterfly-module-2014)
 
 ;(show-butterfly 'red 'stripes 'curved 'rhomb)
 ;(show-butterfly 'yellow 'stripes 'straight 'hexagon)
 ;(show-butterfly 'blue 'dots 'curly 'ellipse )
 
-
+; Grundfunktion
+; Gibt aus einer Liste ein zufälliges Element zurück
 (define (listPickRandomItem l)
   (list-ref l (random (length l))))
 
-(define mendelRegeln
+; Die Definition der Mendelregeln. Dies ist eine Liste mit Unterlisten für 
+; die Farbe, die Musterung, die Fühlerform und die Flügelform.
+; Je weiter links (am Anfang der Liste), desto dominanter ist das Merkmal
+(define mendelRules
   (list
-    '( blue yellow red)
-    '( stripes dots star )
-    '( curved straight curly )
-    '( hexagon ellipse rhomb )
+    (list 'blue 'yellow 'red )
+    (list 'stripes 'dots 'star )
+    (list 'curved 'straight 'curly )
+    (list 'hexagon 'ellipse 'rhomb )
   )
 )
 
-(define (dominant->recessiveList ruleList dominant)
-  (if 
-   (empty? ruleList)
-   '()
-   (if 
-    (eq? (car ruleList) dominant)
-    ruleList
-    (dominant->recessiveList (cdr ruleList) dominant)
-    )
-   )
-)
-
-(define (getRandomPropertiesOfParent ruleListList dominantList)
-  (if 
-      (or (empty? ruleListList) (empty? dominantList))
-      '()
-      (cons
-       (listPickRandomItem (dominant->recessiveList (car ruleListList) (car dominantList)) )
-       (getRandomPropertiesOfParent (cdr ruleListList) (cdr dominantList))
-      )
-  )
-)
-
-(define (choose item1 item2)
-  (if (equal? (random 2) 0)
-      item1
-      item2
-      )
-  )
-
+; Liefert aus das Element aus der needleList zurück, welches als erstes 
+; in der haystackList gefunden wird oder eben die leere Liste.
+; (find-first-of '(a b c) '(c b)) -> 'b
+; (find-first-of '(c a b) '(c b)) -> 'c
+; (find-first-of '(c a b) '(d)) -> '()
 (define (find-first-of haystackList needleList)
   (if
    (empty? haystackList)
@@ -83,22 +66,71 @@
   )
 )
 
-(define (mendel motherDominant fatherDominant anzahl)
+; Zeichnet zu gegebenen mütterlichen und väterlichen dominaten Eigenschaften
+; einen möglichen Kindschmetterling.
+; Die Parameter müssen die Form einer Liste haben, bestehend aus den 4 Elementen:
+;  Farbe, Muster, Fühlerform, Flügelform
+(define (mendel-draw motherDominant fatherDominant)
   (let*
       (
-        [motherRecessive (getRandomPropertiesOfParent mendelRegeln motherDominant)]
-        [fatherRecessive (getRandomPropertiesOfParent mendelRegeln fatherDominant)]
-        [motherRandom (map choose motherDominant motherRecessive)]
-        [fatherRandom (map choose fatherDominant fatherRecessive)]
-        [child (map find-first-of mendelRegeln (map list motherRandom fatherRandom))]
+        ; xxxRecessive:
+        ;  Es werden die Eigenschaften auf die Mendelregeln gemappt. Somit besteht
+        ;  die Ausgabelisten der einzelnen Merkmale nur noch aus möglichen rezessiven
+        ;  Eigenschaften, jedoch nicht mehr aus noch dominanteren.
+        ;  Danach werden die Eigenschaften noch gemappt und ein Element rausgepickt (Zufall),
+        ;  sodass dann eine Liste mit den einzelen rezessiven Eigenschaften übrig bleibt.
+        [motherRecessive (map listPickRandomItem (map member motherDominant mendelRules ))]
+        [fatherRecessive (map listPickRandomItem (map member fatherDominant mendelRules ))]
+        ; xxxRandom:
+        ;  Einfach: Die dominante und rezessive Eigenschaft wird zusammengeworfen und dann
+        ;  ein jeweils eine Eigenschaft zufällig ausgewählt.
+        ;  Genauer: Die beiden Listen mit den dominanten und rezessiven Merkmalen werden
+        ;  verbunden (map list) und dann wie eben zufällig ein Element heraus gepickt.
+        [motherRandom (map listPickRandomItem (map list motherDominant motherRecessive))]
+        [fatherRandom (map listPickRandomItem (map list fatherDominant fatherRecessive))]
+        ; child:
+        ;  Zuerst werden die Eigenschaften von Mutter und Vater gemappt/verbunden. Daraus
+        ;  entsteht wie ebene eine Liste von Paare der Eigenschaften. Hieraus wird nun unter
+        ;  Berücksichtigung der Mendelregeln die dominantere ausgewählt. Dies wird über
+        ;  find-first-of gelöst, dass das frühere/dominantere Element/Eigenschaft in einer
+        ;  Liste findet. Muss für alle Eigenschaften gemacht werden, also map verwenden.
+        [child (map find-first-of mendelRules (map list motherRandom fatherRandom))]
       )
-      (display fatherRecessive)
-      (display fatherRandom)
-      (display (map list fatherRecessive fatherRandom))
-      (display (map find-first-of mendelRegeln (map list motherRandom fatherRandom)))
-      (show-butterfly (car child) (cadr child) (caddr child) (cadddr child))
+    
+      ; Dann nur noch anzeigen.
+      ; Durch die Struktur von child, können hier jetzt nacheinander die einzelnen
+      ; Eigenschaften ausgelesen werden.
+      (display (show-butterfly (car child) (cadr child) (caddr child) (cadddr child) ))
     )
+)
+
+; Zeichnet n (anzahl) Schmetterlingskinder zu gegebenen mütterlichen und väterlichen dominaten Eigenschaften.
+; (Mindestens einer wird gezeichnet)
+; Die Parameter müssen die Form einer Liste haben, bestehend aus den 4 Elementen:
+;  Farbe, Muster, Fühlerform, Flügelform
+(define (mendel motherDominant fatherDominant anzahl)
+  (mendel-draw motherDominant fatherDominant)
+  (if
+   (> anzahl 1)
+   (mendel motherDominant fatherDominant (sub1 anzahl))
+   "Done"
   )
+)
 
 
-(mendel '( blue stripes curved hexagon ) '( yellow dots straight rhomb ) 1)
+(mendel 
+ (list 'blue 'stripes 'curved 'hexagon ) 
+ (list 'yellow 'dots 'straight 'rhomb ) 
+ 2
+)
+(mendel 
+ (list 'yellow 'star 'straight 'rhomb ) 
+ (list 'yellow 'dots 'curly 'rhomb ) 
+ 4
+)
+
+(mendel 
+ (list 'red 'dots 'straight 'rhomb ) 
+ (list 'yellow 'dots 'straight 'ellipse ) 
+ 4
+)
