d9b56c411ec6b53dd001ea6063afbe018a0c0310
likesmusik <3back@informatik.uni-hamburg.de>
2014-11-28 13:50:06 +0100

    Initial Blatt05

---
+++ blatt05/3back3behrend3staeger - blatt 5.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+#|
+  SE3 Funktionale Programmierung
+  WiSe 2014, Uni Hamburg
+  
+  Gruppe 11, Do. 18:15
+  Aufgabenblatt 05
+  
+  Eingereicht von
+    Timon Back (3back)
+    Fabian Behrendt (3behrend)
+    Nicolai Stäger (3staeger)
+
+  Beispielaufrufe befinden sich immer am Ende der Aufgabe
+  und können einfach auskommentiert werden.
+|#
+
+
+
+; ##############################################################################
+; ## Aufgabe 1 #################################################################
+; ##############################################################################
+
+(require se3-bib/butterfly-module-2014)
+
+(show-butterfly 'red 'strpies 'curved 'rhomb)
+(show-butterfly 'yellow 'strpies 'straight 'hexagon)
+(show-butterfly 'blue 'dots 'curly 'ellipse )
