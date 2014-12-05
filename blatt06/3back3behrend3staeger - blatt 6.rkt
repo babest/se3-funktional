6f72b3347116898d5e14f3d6dcac0c279bb9f25b
nstaeger <mail@nstaeger.de>
2014-12-05 13:06:20 +0100

    Blatt 01 Aufgabe 1

--- blatt06/3back3behrend3staeger - blatt 6.rkt
+++ blatt06/3back3behrend3staeger - blatt 6.rkt
@@ -21,3 +21,49 @@
 ; ##############################################################################
 ; ## Aufgabe 1 #################################################################
 ; ##############################################################################
+
+#|
+
+Eine Funktionsdefinition, die sich auf der rechten Seite der definierenden Gleichung
+in jeder Fallunterscheidung selbst nur einmal verwendet, heißt linear-rekursiv.
+
+Eine rekursive Definition ist baumartig wenn in der Definition in einer
+Fallunterscheidung mehrfach auf die Definition Bezug genommen wird.
+
+Eine Rekursion ist geschachtelt, wenn die Funktion in der rekursiven Verwendung
+selbst als Argument mitgegeben wird.
+
+Eine rekursive Definiton heißt indirekt oder verschränkt, wenn zwei oder mehrere
+Definitionen sich wechselseitig rekursiv verwenden.
+
+Rekursive Funktionen, bei denen das Ergebnis der Rekursion nicht mehr mit anderen
+Termen verknüpft werden muß, heißen endrekursiv.
+
+
+# kopfstueck
+- lineare Rekursion
+- direkte Rekursion
+
+# endstueck
+- lineare Rekursion
+- direkte Rekursion
+- End-Rekursion
+
+# merge
+- lineare Rekursion
+- direkte Rekursion
+
+# merge-sort
+- Baumrekursion
+- direkte Rekursion
+
+
+# mögliche antworten
+- lineare Rekursion
+- Baumrekursion
+- geschachtelte Rekursion
+- direkte Rekursion
+- indirekte Rekursion
+- End-Rekursion
+
+|#
