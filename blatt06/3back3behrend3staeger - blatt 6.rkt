60246d93661e95a5bf07528e17330527a211dc92
nstaeger <mail@nstaeger.de>
2014-12-05 12:35:00 +0100

    blatt 06 init

---
+++ blatt06/3back3behrend3staeger - blatt 6.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+#|
+  SE3 Funktionale Programmierung
+  WiSe 2014, Uni Hamburg
+  
+  Gruppe 11, Do. 18:15
+  Aufgabenblatt 06
+  
+  Eingereicht von
+    Timon Back (3back)
+    Fabian Behrendt (3behrend)
+    Nicolai Stäger (3staeger)
+
+  Beispielaufrufe befinden sich immer am Ende der Aufgabe
+  und können einfach auskommentiert werden.
+|#
+
+
+
+; ##############################################################################
+; ## Aufgabe 1 #################################################################
+; ##############################################################################
