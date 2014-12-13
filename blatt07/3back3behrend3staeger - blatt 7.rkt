b1c98a3ea7b97c07430a5321210f1d14d63cd8c1
nstaeger <mail@nstaeger.de>
2014-12-13 15:37:20 +0100

    blatt 07 aufgabe 2.4 + bugfixes

--- blatt07/3back3behrend3staeger - blatt 7.rkt
+++ blatt07/3back3behrend3staeger - blatt 7.rkt
@@ -120,16 +120,18 @@
 ; Scaliert eine Range auf ein neues Interval
 (define (rescale1d source-range destination-interval)
   (let*
-      ( ; Im ersten Schritt normalisieren wir die Liste, dass sie mit 0 anfängt
-       ; Also von jedem Element den Wert des ersten Elements abziehen
-       ; '(2 4 6) -> '(0 2 4) 
-       [ list-zero ( map (curryr - (car source-range)) source-range ) ]
-       ; Im zweiten Schritt normalisieren wir die Liste,
-       ; so dass sie nur Werte von 0 bis 1 enthält
-       ; '(0 2 4) -> '(0 1/2 1)
-       [ list-norm ( map (curryr / (last-element list-zero)) list-zero ) ]
+      ( ; Im ersten Schritt normalisieren wir die Liste, dass das kleinste
+       ; Element der Liste die 0 ist. Anders gesagt: von jedem Element der Liste
+       ; den Wert des kleinsten Elements aus der Liste abziehen.
+       ; '(2 4 6) -> '(0 2 4) und '(0 1 0 -1) -> '(1 2 1 0)
+       [ list-zero ( map (curryr - (argmin (curry + 0) source-range)) source-range ) ]
+       ; Im zweiten Schritt normalisieren wir die Liste, so dass sie nur Werte von
+       ; 0 bis 1 enthält. Anders gesagt, wir teilen jedes Element der Liste
+       ; durch den Wert des größten Elements der Liste
+       ; '(0 2 4) -> '(0 1/2 1) und '(1 2 1 0) -> '(1/2 1 1/2 0)
+       [ list-norm ( map (curryr / (argmax (curry + 0) list-zero)) list-zero ) ]
        ; Im dritten Schritt multiplizieren wir alle Elemente der normierten
-       ; Liste mit der Different des destination-interval
+       ; Liste mit der Different des destination-interval.
        [ list-scaled ( map (curry * (diff destination-interval)) list-norm ) ]
        ; Jetzt müssen wir nur noch zu jedem Element die untere Grenze des
        ; destination-interval hinzuzählen
@@ -141,6 +143,7 @@
 
 ; Beispielaufruf
 ;( rescale1d '(0 2 4 6 8) '(10 . 50) )
+;( rescale1d '(0 1 2 1 0 -1 -2 -1 0) '(0 . 4) )
 
 
 ; Scaliert eine Menge von zweier-Tupeln auf zwei neue Intervalle
@@ -156,6 +159,7 @@
 
 ; Beispielaufruf
 ;( rescale2d '( ( 0 . 0) (2 . 4) (4 . 16) (6 . 36) (8 . 64) ) '(10 . 50) '(5 . 25) )
+;( rescale2d '( ( 0 . 0) (2 . 1) (4 . 0) (6 . -1) (8 . 0) (10 . 1) ) '(0 . 10) '(0 . 2) )
 
 
 
@@ -171,12 +175,13 @@
 ; um die einzelnen Objekte auf dem Bild zu platzieren.
 ( define orientation-canvas ( rectangle scene-width scene-height "solid" ( color 0 0 0 0 ) ) )
 
-
+; Zeichnet eine Liste von Punkten (x-y-Paar) auf die Zeichenfläche
 (define (draw-points pointlist)
   (let*
+      ; Den Punkt berechnen mit seiner Position (x: erstes Element vom ersten Tupel der Liste...)
       ( [ point (place-image (circle 1 "solid" "blue") (caar pointlist) (- scene-height (cdar pointlist)) orientation-canvas) ] )
-    ( cond 
-       [ (empty? pointlist) ]
+    ( cond
+       ; Wenn nur noch ein Punkt in der Liste ist...
        [ (= (length pointlist) 1) point ]
        [ else ( underlay
                 point
@@ -187,7 +192,28 @@
   )
 
 ; Beispielaufruf
-;(draw-points (function->points sqr '(0 . 50) 200))
-;(draw-points (function->points (curry * 0.5) '(0 . 800) 300))
+;( draw-points (function->points sqr '(0 . 50) 200) )
+;( draw-points (function->points (curry * 0.5) '(0 . 800) 300) )
 
 
+
+; ##############################################################################
+; ## Aufgabe 2.4 ###############################################################
+; ##############################################################################
+
+(define (plot-function func interval n)
+  ( draw-points
+    ( rescale2d
+      ( function->points func interval n )
+      ( cons 0 scene-width)
+      ( cons 0 scene-height)
+      )
+    )
+  )
+
+; Beispielaufruf
+;( plot-function sin (cons 0 (* 2 pi)) 200)
+;( plot-function cos (cons 0 (* 2 pi)) 200)
+;( plot-function tan '(-1.5 . 1.5) 200)
+;( plot-function sqr '(-10 . 10) 200)
+;( plot-function sqrt '(0 . 10) 200)
