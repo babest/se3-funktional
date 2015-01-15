e3f447000b93a083dbc69b615b977142b23283ec
nstaeger <mail@nstaeger.de>
2015-01-15 15:05:53 +0100

    blatt 10 init

---
+++ blatt10/3back3behrend3staeger - blatt 10.rkt
@@ -0,0 +1,20 @@
+#lang swindle
+
+#|
+  SE3 Funktionale Programmierung
+  WiSe 2014, Uni Hamburg
+  
+  Gruppe 11, Do. 18:15
+  Aufgabenblatt 10
+  
+  Eingereicht von
+    Timon Back (3back)
+    Fabian Behrendt (3behrend)
+    Nicolai Stäger (3staeger)
+
+  Beispielaufrufe befinden sich immer am Ende der Aufgabe
+  und können einfach auskommentiert werden.
+|#
+
+
+
