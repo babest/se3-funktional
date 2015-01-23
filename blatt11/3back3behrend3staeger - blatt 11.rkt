b98d80a21f3b08f259cf76f300061284c1fbf9cf
nstaeger <mail@nstaeger.de>
2015-01-23 12:03:48 +0100

    blatt 11 init

---
+++ blatt11/3back3behrend3staeger - blatt 11.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+#|
+  SE3 Funktionale Programmierung
+  WiSe 2014, Uni Hamburg
+  
+  Gruppe 11, Do. 18:15
+  Aufgabenblatt 11
+  
+  Eingereicht von
+    Timon Back (3back)
+    Fabian Behrendt (3behrend)
+    Nicolai Stäger (3staeger)
+
+  Beispielaufrufe befinden sich immer am Ende der Aufgabe
+  und können einfach auskommentiert werden.
+|#
+
+
+
+; ##############################################################################
+; ## Aufgabe 1 #################################################################
+; ##############################################################################
+
